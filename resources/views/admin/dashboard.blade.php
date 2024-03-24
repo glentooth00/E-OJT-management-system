1c38e79efb4d5e0e6c1ef18591849d0a43f59e35
mantungalvin21 <98724589+mantungalvin21@users.noreply.github.com>
2024-03-25 07:58:37 +0800

    WIP

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,19 @@
+@extends('includes.layouts.app')
+
+
+@section('content')
+
+      <!-- Container Fluid-->
+        <div class="container-fluid" id="container-wrapper">
+            <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                <h1 class="h3 mb-0 text-gray-800">Blank Page</h1>
+            </div>
+
+            <div class="text-center">
+                <img src="img/think.svg" style="max-height: 90px">
+                <h4 class="pt-3">save your <b>imagination</b> here!</h4>
+            </div>
+        </div>
+        <!---Container Fluid-->
+
+@endsection
