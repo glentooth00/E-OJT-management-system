7c65d81b0a1df32847283f5c65f65311500cbcea
mantungalvin21 <98724589+mantungalvin21@users.noreply.github.com>
2024-03-25 09:45:48 +0800

    WIP

--- resources/views/includes/menus/side.blade.php
+++ resources/views/includes/menus/side.blade.php
@@ -1,14 +1,11 @@
  <!-- Sidebar -->
     <ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
       <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div class="sidebar-brand-icon">
-          <img src="img/logo/logo2.png">
-        </div>
-        <div class="sidebar-brand-text mx-3">RuangAdmin</div>
+        <div class="sidebar-brand-text mx-3">E-OJT Management System</div>
       </a>
       <hr class="sidebar-divider my-0">
       <li class="nav-item">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="/">
           <i class="fas fa-fw fa-tachometer-alt"></i>
           <span>Dashboard</span></a>
       </li>
@@ -18,8 +15,58 @@
       </div>
       <li class="nav-item">
         <a class="nav-link" href="ui-colors.html">
-          <i class="fas fa-fw fa-palette"></i>
-          <span>UI Colors</span>
+          <i class="fas fa-fw fa-users"></i>
+          <span>Interns</span>
+        </a>
+      </li> 
+      <li class="nav-item">
+        <a class="nav-link" href="ui-colors.html">
+          <i class="fas fa-fw fa-users"></i>
+          <span>Pending Interns</span>
+        </a>
+      </li> 
+      <li class="nav-item">
+        <a class="nav-link" href="ui-colors.html">
+          <i class="fas fa-fw fa-file"></i>
+          <span>Intern's Evaluation</span>
+        </a>
+      </li> 
+      <li class="nav-item">
+        <a class="nav-link" href="ui-colors.html">
+          <i class="fas fa-fw fa-clock"></i>
+          <span>Intern's Logs</span>
+        </a>
+      </li> 
+      <li class="nav-item">
+        <a class="nav-link" href="ui-colors.html">
+          <i class="fas fa-regular fa-building"></i>
+          <span>MOA / MOU</span>
+        </a>
+      </li> 
+      <li class="nav-item">
+        <a class="nav-link" href="ui-colors.html">
+          <i class="fas fa-solid fa-book"></i>
+          <span>Bookmarks</span>
+        </a>
+      </li> 
+      <li class="nav-item">
+        <a class="nav-link" href="ui-colors.html">
+          <i class="fas fa-solid fa-bell"></i>
+          <span>Notification</span>
+          <span class="badge badge-success rounded-circle" style="font-size: 10px;">3+</span>
+        </a>
+      </li> 
+        <hr class="sidebar-divider">
+      <li class="nav-item">
+        <a class="nav-link" href="ui-colors.html">
+          <i class="fas fa-solid fa-cogs"></i>
+          <span>Settings</span>
+        </a>
+      </li> 
+      <li class="nav-item">
+        <a class="nav-link" href="ui-colors.html">
+        <i class="fas fa-sign-out-alt"></i>
+          <span>Logout</span>
         </a>
       </li> 
       <div class="sidebar-heading">
--- resources/views/includes/sections/header.blade.php
+++ resources/views/includes/sections/header.blade.php
@@ -8,7 +8,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
   <link href="img/logo/logo.png" rel="icon">
-  <title>RuangAdmin - Blank Page</title>
+  <title>E-OJT</title>
   <link href="{{ asset('admin/css/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
   <link href="{{ asset('admin/css/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
   <link href="{{ asset('admin/css/style/ruang-admin.min.css')}}" rel="stylesheet">
