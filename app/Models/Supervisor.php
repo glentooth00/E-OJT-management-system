2f779b78b35abe6f06e16f1f120a88bb27c87507
Glen Donn B. Alpasan <62196184+glentooth00@users.noreply.github.com>
2024-11-13 08:29:34 +0800

    wip

--- app/Models/Supervisor.php
+++ app/Models/Supervisor.php
@@ -12,7 +12,7 @@ class Supervisor extends Authenticatable
         'first_name',
         'middle_name',
         'last_name',
-        'email',
+        'username',
         'password',
         'office',
         'category',
